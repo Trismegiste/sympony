e79c1c548b8aa303bbbb54d6829c53c500ae8e15
Florent <florent.g@allopneus.com>
2013-03-05 09:48:06 +0100

    kernel ok

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,6 +5,7 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+
     public function registerBundles()
     {
         $bundles = array(
@@ -13,6 +14,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Trismegiste\FrontBundle\TrismegisteFrontBundle(),
+            new Trismegiste\DokudokiBundle\TrismegisteDokudokiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -25,6 +28,34 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
+        $loader->import(__DIR__ . '/config/plateform/' . php_uname('n') . '.yml');
     }
+
+    /**
+     * Injecting the owner's name of this AppKernel file to add an "env parameter"
+     * Note : This mechanism is only avalaible in DEV because it is only intended
+     * in development environment.
+     *
+     * @return array
+     */
+    protected function getEnvParameters()
+    {
+        $base = parent::getEnvParameters();
+        // owner of source
+        if ('dev' == $this->getEnvironment()) {
+            $base['developer.name'] = 'dev';
+
+            if (function_exists('posix_getpwuid')) {
+                $owner = posix_getpwuid(fileowner(__FILE__));
+                if (array_key_exists('name', $owner)) {
+                    $base['developer.name'] = $owner['name'];
+                }
+            }
+        }
+
+
+        return $base;
+    }
+
 }
